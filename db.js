670755a6834809c4bf136e8e0aa168f3876e4d21
Sayed Afnan Khazi <83779299+Sayed-Afnan-Khazi@users.noreply.github.com>
2025-02-28 14:55:21 +0530

    changed to pool mode

--- db.js
+++ db.js
@@ -2,12 +2,15 @@ import pg from 'pg';
 import dotenv from 'dotenv';
 dotenv.config();
 
-const db = new pg.Client({
+const db = new pg.Pool({
+    pool_mode: process.env.POOL_MODE,
     host : process.env.POSTGRES_HOST,
     user : process.env.POSTGRES_USER,
     database : process.env.POSTGRES_DATABASE,
     password : process.env.POSTGRES_PASSWORD,
-    port : process.env.POSTGRES_PORT
+    port : process.env.POSTGRES_PORT,
+    idleTimeoutMillis: 30000, // Auto-close idle connections after 30 seconds
+    connectionTimeoutMillis: 2000, // Fail fast on connection timeout
 });
 
 db.connect()
